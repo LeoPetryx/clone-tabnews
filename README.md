6dad25499559e0aff7bb5a5a303e25599130851a
Leonardo <130802152+LeoPetryx@users.noreply.github.com>
2024-11-05 18:18:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Primeiro projeto de desenvolvimento para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Tomara que essa aula de certo!</h1>
+  return <h1>Tomara que essa aula de certo!</h1>;
 }
 
-export default Home; 
+function Teste() {
+  console.log("teste.");
+}
+
+export default Home;
